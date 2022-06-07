b0ed36a61e272af8434809ee776cadeae83e0829
Jason L <jkleitz27@gmail.com>
2022-06-07 19:44:09 -0400

    zookeeper updates

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
-router.use(require('./zookeeperRoutes'));
+const zookeeperRoutes = require('../apiRoutes/zookeeperRoutes');
 
 router.use(animalRoutes);
+router.use(zookeeperRoutes);
 
 module.exports = router;
